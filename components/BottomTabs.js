44fa92749a887501f9ae857cb3841f99c724a040
zielinsky <mrzbiznes@gmail.com>
2022-09-04 16:35:27 +0200

    Add new icon for BottomTab

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,32 +1,72 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import { Divider } from 'react-native-elements'
 
 export const bottomTabIcons = [
     {
-      name: 'Profil',
-      path: '../assets/icons/profile.png'
-    },
-    {
-      name: 'Historia',
-      path: '../assets/icons/profile.png'
+      name: 'Plany',
+      path: require('../assets/icons/plans.png')
     },
     {
       name: 'Progress',
-      path: '../assets/icons/profile.png'
+      path: require('../assets/icons/progress.png')
     },
     {
-      name: 'Plany',
-      path: '../assets/icons/profile.png'
+      name: 'Rozpocznij trening',
+      path: require('../assets/icons/play.png')
+    },
+    {
+      name: 'Historia',
+      path: require('../assets/icons/history.png')
+    },
+    {
+      name: 'Profil',
+      path: require('../assets/icons/profile.png')
     },
 ]
 
 
-const BottomTabs = () => {
+const BottomTabs = ({icons}) => {
+  const [activeTab, setActiveTab] = useState('Home')
+
+  const Icon = ({icon}) => (
+    <TouchableOpacity onPress={() => setActiveTab(icon.name)}>
+
+      <Divider style={styles.iconContainer}>
+        <Image
+          source={icon.path} 
+          style={styles.icon}
+        />
+        <Text> {icon.name} </Text>
+      </Divider>
+      
+    </TouchableOpacity>
+  )
   return (
-    <View>
-      <Text>BottomTabs</Text>
+    <View style={styles.iconsContainer}>
+      {icons.map((icon, index) => (
+        <Icon key={index} icon={icon} />
+      ))}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  iconsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  iconContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    resizeMode: 'contain'
+  },
+})
 
 export default BottomTabs
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
-import { Text } from 'react-native'
 import React from 'react'
+import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import BottomTabs, { bottomTabIcons } from '../components/BottomTabs'
 const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <Text>Home Screen</Text>
+      <BottomTabs icons={bottomTabIcons}/>
     </SafeAreaView>
   )
 }
