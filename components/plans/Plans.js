eac4040c3cb8d5b671ac9c580868b975651967ee
zielinsky <mrzbiznes@gmail.com>
2022-09-05 16:47:24 +0200

    Change font size

--- components/plans/Plans.js
+++ components/plans/Plans.js
@@ -16,35 +16,29 @@ const Plans = () => {
   return (
     <View style={styles.container}>
       <TopTabs />
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
         <TouchableOpacity  style={styles.buttonContainer} >
           <Text style={styles.text}>
-            Moje plany
+            MOJE PLANY
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity  style={styles.buttonContainer} >
-          <Text style={styles.text}>
-            Moje plany
-          </Text>
-        </TouchableOpacity>
-      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   text: {
+    fontSize: 25,
     color: 'white'
   },
   buttonContainer: {
     paddingTop: 15,
     paddingBottom: 15,
-    paddingLeft: 40,
-    paddingRight: 40,
+    paddingLeft: 70,
+    paddingRight: 70,
     bottom: 80,
     borderWidth: 1,
     borderRadius: 15,
-    backgroundColor: '#4E6889'
+    backgroundColor: '#132235'
   },
   container: {
     height: '100%',
