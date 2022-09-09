301661cd9cf9502e21bb8eb4eebd2fcefda8856f
zielinsky <mrzbiznes@gmail.com>
2022-09-09 15:55:53 +0200

    Add login screen and firebase connection

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import * as React from "react";
 import MainScreen from './screens/MainScreen';
+import LoginScreen from './screens/LoginScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <MainScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Logowanie" component={LoginScreen}/>
+        <Stack.Screen options={{headerShown: false}} name="Main" component={MainScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- components/mainscreen/history/History.js
+++ components/mainscreen/history/History.js
@@ -1,11 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 const History = () => {
   return (
-    <View>
-      <Text>History</Text>
-    </View>
+    <Calendar />
   )
 }
 
