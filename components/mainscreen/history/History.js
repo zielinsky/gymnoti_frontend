24e205e7ea47798cb1d315d41fbe997e6449d0d0
zielinsky <mrzbiznes@gmail.com>
2022-09-12 21:35:29 +0200

    Finish History training display

--- components/mainscreen/history/History.js
+++ components/mainscreen/history/History.js
@@ -1,38 +1,77 @@
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native'
 import { Divider } from 'react-native-elements'
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Calendar } from 'react-native-calendars';
-// import {LocaleConfig} from 'react-native-calendars';
+import {LocaleConfig} from 'react-native-calendars';
+import { collection, doc, getDoc, onSnapshot } from 'firebase/firestore'
+import {firestore, auth} from '../../../firebase'
 
-// LocaleConfig.locales['pl'] = {
-//   monthNames: [
-//     'Styczeń',
-//     'Luty',
-//     'Marzec',
-//     'Kwiecień',
-//     'Maj',
-//     'Czerwiec',
-//     'Lipiec',
-//     'Sierpień',
-//     'Wrzesień',
-//     'Październik',
-//     'Listopad',
-//     'Grudzeń'
-//   ],
-//   monthNamesShort: ['STY', 'LUT', 'MAR', 'KWI', 'MAJ', 'CZE', 'LIP.', 'SIE', 'WRZ', 'PAŹ', 'LIS.', 'GRU'],
-//   dayNames: ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'],
-//   dayNamesShort: ['Pon.', 'Wt.', 'Śr.', 'Czw.', 'Pt.', 'Sob.', 'Ndz.']
-// };
-// LocaleConfig.defaultLocale = 'pl';
+LocaleConfig.locales['pl'] = {
+  monthNames: [
+    'Styczeń',
+    'Luty',
+    'Marzec',
+    'Kwiecień',
+    'Maj',
+    'Czerwiec',
+    'Lipiec',
+    'Sierpień',
+    'Wrzesień',
+    'Październik',
+    'Listopad',
+    'Grudzeń'
+  ],
+  monthNamesShort: ['STY', 'LUT', 'MAR', 'KWI', 'MAJ', 'CZE', 'LIP.', 'SIE', 'WRZ', 'PAŹ', 'LIS.', 'GRU'],
+  dayNames: ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'],
+  dayNamesShort: ['Pon.', 'Wt.', 'Śr.', 'Czw.', 'Pt.', 'Sob.', 'Ndz.']
+};
+LocaleConfig.defaultLocale = 'pl';
+
+const HIGHLITED_COLOR = '#2BF1E1'
 
 
 const History = () => {
+  const [loading, setLoading] = useState(true)
+  const [trainings, setTrainings] = useState({})
 
-  const Training = ({training}) => (
+  useEffect(() => {
+
+    const getDocsReference = async ({trainingsRef}) => {
+        const itemsDocs = await Promise.all(trainingsRef.map(item => getDoc(item['training'])))
+        const trainingsSnap = [] 
+        
+        itemsDocs.map((item, index) => trainingsSnap.push([item.data(), trainingsRef[index]["date"]]))
+
+        setTrainings(trainingsSnap);
+        setLoading(false);
+    }
+
+    const  subscriber = onSnapshot(collection(firestore, 'users', auth.currentUser.uid, 'trainings'), QuerySnapshot => {
+
+      const trainingsRef = []
+      QuerySnapshot.forEach(documentSnapshot => {
+        trainingsRef.push(documentSnapshot.data())
+      })
+
+      getDocsReference({trainingsRef: trainingsRef})
+
+    });
+    return () => subscriber()
+  }, [])
+
+  if(loading)
+    return <ActivityIndicator size="large" color={HIGHLITED_COLOR} style={{flex: 1}}/>
+
+
+  const dateFormat = (date) =>
+                            date.getDate() + ' ' + LocaleConfig.locales['pl'].monthNames[date.getMonth()+1] +' ' + date.getFullYear() +
+                            ' ' + date.toLocaleTimeString()
+
+  const Training = ({training, date}) => (
     <TouchableOpacity style={styles.dayWrapper}>
       <View style={styles.dayContainer}>
         <Text style={{paddingLeft: 15}}> {training} </Text>
+        <Text style={{paddingLeft: 15}}> {dateFormat(date)} </Text>
       </View>
     </TouchableOpacity>
   )
@@ -42,37 +81,10 @@ const History = () => {
         <Calendar />
         <Divider width={1} orientation='vertical' />
         <View style={{height: '53%', alignItems: 'center'}} >
-          <ScrollView >
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
-              <Training training="elo"/>
+          <ScrollView>
+            {trainings.map((training, index) => (
+              <Training key={index} training={training[0].name} date={training[1].toDate('en-EN')}/>
+            ))}
           </ScrollView>
         </View>
       </View>
