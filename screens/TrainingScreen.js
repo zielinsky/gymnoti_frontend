b1dfb4ff52b1ac513792723b8f937c662c344cc9
zielinsky <mrzbiznes@gmail.com>
2022-09-15 12:54:14 +0200

    Add TrainingScreen

--- screens/TrainingScreen.js
+++ screens/TrainingScreen.js
@@ -1,16 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useRoute } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const TrainingScreen = () => {
-const route = useRoute()
-  return (
-    <View>
-      
-    </View>
-  )
+  const [exerciseID, setExerciseID] = useState()
+  const [setID, setSetID] = useState()
+  
+  const route = useRoute()
+
+    return (
+      <SafeAreaView style={styles.container}>
+        
+      </SafeAreaView>
+    )
 }
 
 export default TrainingScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    flexGrow: 1,
+    justifyContent: 'space-between',
+    width: '100%',
+    backgroundColor: '#08090D',
+  }
+})
